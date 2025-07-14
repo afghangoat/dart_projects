05f90a63610fc413b1a84af20dbc035b57e9f475
AfghanGoat <ifj.funk.gabor@gmail.com>
2025-07-14 08:16:54 +0200

    Initial commit - Mirrored from my site, file: dart_projects/01_intro.dart

---
+++ dart_projects/01_intro.dart
@@ -0,0 +1,7 @@
+//Basic dart syntax showcase
+void main() {
+  for (int i = 0; i < 10; i++) {
+    //Dart has optionally typing so you can use "var" or "int" or a concrete type.
+    print('hello ${i + 1}'); //Printing works simillar to JS
+  }
+}
